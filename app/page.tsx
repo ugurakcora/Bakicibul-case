361ed516636733e8a21dfc0ba11feb6423cd0f8e
ugurakcora <uakcora97@gmail.com>
2024-10-25 18:37:25 +0300

    fix: deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-// app/page.tsx
 import CharacterFilter from "./components/CharacterFilter";
 import CharacterList from "./components/CharacterList";
 import { fetchCharacters } from "./api/fetchCharacters";
@@ -14,18 +13,18 @@ export interface Character {
 const HomePage = async ({
   searchParams,
 }: {
-  searchParams: { status?: string; gender?: string };
+  searchParams: Promise<{ status?: string; gender?: string }>;
 }) => {
-  const { status, gender } = await searchParams;
-  const initialCharacters: Character[] = await fetchCharacters({
-    status,
-    gender,
-  });
+  const params = await searchParams;
+  const initialCharacters: Character[] = await fetchCharacters(params);
 
   return (
     <div className="p-8">
       <h1 className="text-2xl font-bold mb-4">Rick and Morty Characters</h1>
-      <CharacterFilter initialStatus={status} initialGender={gender} />
+      <CharacterFilter
+        initialStatus={params.status}
+        initialGender={params.gender}
+      />
       <CharacterList characters={initialCharacters} />
     </div>
   );
