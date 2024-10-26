7403a53dab08e00744549109badf4c8a932900dc
ugurakcora <uakcora97@gmail.com>
2024-10-26 12:59:15 +0300

    fix: deploy

--- app/page.tsx
+++ app/page.tsx
@@ -17,16 +17,14 @@ interface HomePageProps {
 export default async function HomePage({
   searchParams: initialSearchParams,
 }: HomePageProps) {
-  const searchParams = await initialSearchParams;
-  const initialCharacters: Character[] = await fetchCharacters(searchParams);
+  const { status, gender } = await initialSearchParams;
+
+  const initialCharacters = await fetchCharacters({ status, gender });
 
   return (
     <div className="p-8">
       <h1 className="text-2xl font-bold mb-4">Rick and Morty Characters</h1>
-      <CharacterFilter
-        initialStatus={searchParams.status}
-        initialGender={searchParams.gender}
-      />
+      <CharacterFilter initialStatus={status} initialGender={gender} />
       <CharacterList characters={initialCharacters} />
     </div>
   );
